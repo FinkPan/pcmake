59d58f14907d0705101d899a9f8ac5d27888f276
fink <hspan1234@gmail.com>
2014-05-12 17:58:06 +0800

    完成自己的cmake宏.
    Signed-off-by: fink <hspan1234@gmail.com>

--- cmake/hsprj.cmake
+++ cmake/hsprj.cmake
@@ -1,4 +1,5 @@
 macro(prjenv)
+
   option(HSLIB_INSTALL_DIR_OPTION "OFF: use System Path Value: 3RDPARTY_INSTALL_DIR." OFF)
   option(HSLIB_BUILD_SHARED_OPTION "Check if link shared hs library" OFF)
   #if build the shared lib or not?
@@ -11,19 +12,22 @@ macro(prjenv)
 	
 	#if set the lib install dir
 	if(NOT HSLIB_INSTALL_DIR_OPTION)
-		#$ENV{3RDPARTY_INSTALL_DIR} NAME: 3RDPARTY_INSTALL_DIR
+		#默认:HSLIB_INSTALL_DIR_OPTION为 OFF : 搜索系统环境变量:3RDPARTY_INSTALL_DIR
+    #如果不存在3RDPARTY_INSTALL_DIR, 则提示用户设定3RDPARTY_INSTALL_DIR
 		set(HSLIB_DIR $ENV{3RDPARTY_INSTALL_DIR})
 			if(NOT HSLIB_DIR)
 				message("can't find the 3RDPARTY_INSTALL_DIR path.\nplease set the \"HSLIB_DIR\"")
-				set(HSLIB_DIR "" CACHE PATH "3rdparty libs install directory")
+				set(HSLIB_DIR CACHE PATH "3rdparty libs install directory")
 			endif()
 		else()
-		  set(HSLIB_DIR "" CACHE PATH "3rdparty libs install directory")
+      message("The 3RDPARTY_INSTALL_DIR is null, please set a DIR, or use default DIR.")
+		  set(HSLIB_DIR CACHE PATH "3rdparty libs install directory")
 	endif()
-
+  
 	string(REPLACE  " " "_" GENERATOR_TYPE ${CMAKE_GENERATOR})
-	string(REPLACE  "\\" "/" HSLIB_DIR ${HSLIB_DIR})
+
 	if(HSLIB_DIR)
+    string(REPLACE  "\\" "/" HSLIB_DIR ${HSLIB_DIR})
 		set(CMAKE_INSTALL_PREFIX ${HSLIB_DIR})
 	endif()
 
@@ -32,99 +36,88 @@ endmacro()
 
 #usage: build_lib(lib_name lib_files1 lib_files2...)
 macro(build_lib lib_name)
+
 	prjenv()
-	
 	#config header.
-    configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/config/hs_config.hpp.in"
-                   "${CMAKE_CURRENT_BINARY_DIR}/inc/config/hs_config.hpp")
+  configure_file("${CMAKE_CURRENT_SOURCE_DIR}/cmake/config/hs_config.hpp.in"
+                 "${CMAKE_CURRENT_BINARY_DIR}/inc/config/hs_config.hpp")
 	
 	include_directories(inc)
 	include_directories("${CMAKE_CURRENT_BINARY_DIR}/inc")
 	add_library(${lib_name} ${ARGN})
 	
-	install_lib(${lib_name})
-  
 endmacro()
 
-
+#usage  lib_dep(target_name dep_lib_name)
+#       lib_dep(target_name dep_lib_name DEP lib1 lib2)
+#说明:一次只能添加一个项目的lib
 macro(lib_dep target dep_lib_name)
+
 prjenv()
+set(DEP_LIB_INC "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${dep_lib_name}/inc")
+set(DEP_LIB_DIR "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${dep_lib_name}/lib/$<CONFIGURATION>")
+set(DEP_DLL_DIR "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${dep_lib_name}/bin/$<CONFIGURATION>")
 
-#set(DEP_LIB_INC "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${dep_lib_name}/inc")
-#set(DEP_LIB_DIR "F:/programs/lib/3rdparty/static/Visual_Studio_10/lib1/lib/Debug")
-include_directories(${DEP_LIB_INC})
-target_link_libraries(${target} ${dep_lib_name})
-
-message(${target})					  
-#foreach(conf ${CMAKE_CONFIGURATION_TYPES})
-#target_link_libraries(${lib_name}
-
-#endforeach()					  
-
-
-endmacro()
-
-macro(build_exe exe_name)
-  set(var_list "${ARGN}")
-  set(LIBS "")
-  set(SOURCE_FILES "")
-  foreach(var ${var_list})
-    if(var STREQUAL "DEP")
-      set(NEED_DEP ON)
-    endif()
-    if(NEED_DEP)
-      list(APPEND LIBS ${var})
-    else()
-      list(APPEND SOURCE_FILES ${var})
-    endif()
-  endforeach()
-  list(REMOVE_AT LIBS 0)
-
-  if(NEED_DEP)
-    prjenv()
-    
-    set(DEP_LIB_INC "")
-    set(DEP_LIB_DIR "")
-    set(DEP_LIB_DLL "")
-    foreach(LIB_NAME ${LIBS})
-      list(APPEND DEP_LIB_INC "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${LIB_NAME}/inc")
-      list(APPEND DEP_LIB_DIR "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${LIB_NAME}/lib")
-      list(APPEND DEP_LIB_DLL "${HSLIB_DIR}/${LINK_TYPE}/${GENERATOR_TYPE}/${LIB_NAME}/bin")
-      if(HSLIB_BUILD_SHARED_OPTION)
-       install(FILE "${DEP_LIB_DLL}/${BUILD_TYPE}/${LIB_NAME}.dll"
-              DESTINATION "
-      endif()
-    endforeach()
-
-    
-    include_directories(${DEP_LIB_INC})
-    link_directories(${DEP_LIB_DIR})
-    include_directories(inc)
-    add_executable(${exe_name} ${SOURCE_FILES})
-    target_link_libraries(${exe_name} ${LIBS})
-  else()  
-    include_directories(inc)
-    add_executable(${exe_name} ${ARGN})
+#判断是否有多个libs
+foreach(var_lib ${ARGN})
+  if(var_lib STREQUAL "DEP")
+    set(MULTI_LIB ON)
   endif()
+endforeach()
+
+#多个libs
+if(MULTI_LIB)
+  set(DEP_KEYWORD "${ARGN}")
+  list(REMOVE_ITEM DEP_KEYWORD "DEP")
+  set(LIBS "")
+  set(DLLS "")
+  foreach(var_lib1 ${DEP_KEYWORD})
+    list(APPEND LIBS ${DEP_LIB_DIR}/${var_lib1}.lib)
+    list(APPEND DLLS ${DEP_DLL_DIR}/${var_lib1}.dll)
+  endforeach()
+else() #单个lib
+  set(LIBS ${DEP_LIB_DIR}/${dep_lib_name}.lib)
+  set(DLLS ${DEP_DLL_DIR}/${dep_lib_name}.dll)
+endif()
+
+include_directories(${DEP_LIB_INC})
+target_link_libraries(${target} ${LIBS})
+
+#如果是动态链接,则将dll拷贝到build对应CONFIGURATION目录
+if(BUILD_SHARED_LIBS)
+  foreach(dll_name ${DLLS})
+	add_custom_command(
+          TARGET ${target}
+          POST_BUILD
+          COMMAND ${CMAKE_COMMAND}
+          -E copy ${dll_name} "${CMAKE_CURRENT_BINARY_DIR}/$<CONFIGURATION>/")
+	endforeach()
+endif()
 
 endmacro()
 
-macro(install_lib lib_name)
+#usage build_exe(exe_name source1 source2...)
+macro(build_exe exe_name)
+  include_directories(inc)
+  add_executable(${exe_name} ${ARGN})
+endmacro()
 
+#usage install_lib(lib_name)
+macro(install_lib lib_name)
+  prjenv()
 	#install header files
 	install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/inc/config"
-			DESTINATION "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${lib_name}/inc")
+			DESTINATION "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${PROJECT_NAME}/inc")
 	install(DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/inc/"
-			DESTINATION "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${lib_name}/inc")
+			DESTINATION "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${PROJECT_NAME}/inc")
 		
 	#install lib dll
 	foreach(conf ${CMAKE_CONFIGURATION_TYPES})
 	install(TARGETS ${lib_name}
-			ARCHIVE DESTINATION "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${lib_name}/lib/${conf}"  #libs
+			ARCHIVE DESTINATION "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${PROJECT_NAME}/lib/${conf}"  #libs
 			CONFIGURATIONS ${conf} OPTIONAL
-			RUNTIME DESTINATION  "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${lib_name}/bin/${conf}"  #dlls
+			RUNTIME DESTINATION  "${CMAKE_INSTALL_PREFIX}/${LINK_TYPE}/${GENERATOR_TYPE}/${PROJECT_NAME}/bin/${conf}"  #dlls
 			CONFIGURATIONS ${conf} OPTIONAL) 
-					
 	endforeach()
 
 endmacro()
